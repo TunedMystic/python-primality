ee4fb1f6511bf538ebfb9533bfa9fc90e39c383c
Sandeep Jadoonanan <TunedMystic@users.noreply.github.com>
2015-02-09 19:49:53 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import sys
+import math
+import unittest
+
+"""
+--- Assignment ---
+Write a program that prompts the user to enter an integer.
+If the input value is not an integer greater than one, display
+a helpful error message. Otherwise, output 'prime' if the integer
+is prime (divisible by 1 and itself), or 'composite' if the
+integer is not prime.
+
+To run program:
+  > python main.py
+  
+To run tests:
+  > python main.py test
+"""
